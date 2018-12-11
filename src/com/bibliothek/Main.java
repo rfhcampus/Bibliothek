fa1e6fb381b12df2aab428ada6781719c6f3091f
rfhcampus <45794055+rfhcampus@users.noreply.github.com>
2018-12-11 22:45:24 +0100

    Erster grober Entwurf mit Menü und einer noch einfachen Ausgabe von Benutzerparametern.

--- src/com/bibliothek/Main.java
+++ src/com/bibliothek/Main.java
@@ -1,11 +1,48 @@
 package com.bibliothek;
 
+import java.util.Scanner;
+
 public class Main {
-    //Variablen deklarieren
 
     public static void main(String[] args) {
-	User Mueller = new User();
-	User.addUser();
 
+        System.out.println ("Bitte tätigen Sie Ihre Eingabe!");
+        System.out.println ("-------------------------------");
+        System.out.println ("Taste 1: Benutzer anlegen");
+        System.out.println ("Taste 2: Benutzer sperren");
+        System.out.println ("Taste 3: Buch ausleihen");
+        System.out.println ("Taste 4: Benutzer vormerken");
+        System.out.println ("Taste 5: Buch zurück geben");
+        System.out.println ("-------------------------------");
+
+        Scanner sc = new Scanner (System.in);
+        int i = sc.nextInt ();
+
+        switch (i) {
+
+            case 0:
+                break;
+
+            case 1:
+                User newUser = new User ( );
+                newUser.addUser ( );
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                break;
+
+        }
     }
 }
--- src/com/bibliothek/User.java
+++ src/com/bibliothek/User.java
@@ -1,14 +1,30 @@
 package com.bibliothek;
 
+import java.util.Scanner;
+
 public class User {
 
     //Variablen deklarieren
+
     private String address;
     private String name;
-    private boolean locked;
+    private boolean locked = false;
     private int userid;
+    private int lockedInt;
 
     void addUser() {
-        System.out.println("Nutzer wird (bald) angelegt...");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println ("Bitte den Namen des anzulegenden Benutzers eingeben: ");
+        name = sc.nextLine ();
+        System.out.println ("Bitte den initialen Status für diesen Nutzer eingeben (0=freigegeben | 1=gesperrt): ");
+        lockedInt = sc.nextInt ();
+        if (lockedInt == 1);
+        locked = true;
+
+        System.out.println("Nutzer " + name + " mit Sperrstatus " + locked + " wird (bald) angelegt...");
+
+
+
     }
 }
