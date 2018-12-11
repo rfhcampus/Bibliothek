258d75f4b1ec76ccf69526c60f0d9d51386bac28
rfhcampus <45794055+rfhcampus@users.noreply.github.com>
2018-12-11 19:15:06 +0100

    Zweite Version. Einfach mal zum Testen. // Linus

--- src/com/bibliothek/Benutzer.java
+++ src/com/bibliothek/Benutzer.java
@@ -1,4 +1,15 @@
 package com.bibliothek;
 
 public class Benutzer {
+
+    //Variablen deklarieren
+    private String address;
+    private String name;
+    private boolean locked;
+    private int userid;
+    
+
+
+
+    public static
 }
--- src/com/bibliothek/Main.java
+++ src/com/bibliothek/Main.java
@@ -3,6 +3,6 @@ package com.bibliothek;
 public class Main {
 
     public static void main(String[] args) {
-	
+	System.out.println("Hallo Team!");
     }
 }
